3430cb40ee725b70a98629184e9ed486fa12fb5b
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-23 17:10:43 +0530

    updated-server-code

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const { chats } = require("./data/chat");
+const connectDb = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 const app = express();
+connectDb();
+app.use(express.json()); //To accept JSON Data
+
 const PORT = process.env.PORT;
 
 app.get("/", (req, resp) => {
   resp.send("API is running");
 });
 
-app.get("/api/chat", (req, resp) => {
-  resp.send(chats);
-});
-
-app.get("/api/chat/:id", (req, resp) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  resp.send(singleChat);
-});
+app.use("/api/user/", userRoutes);
 app.listen(PORT, () => {
   console.log(`Server is up and running on port...${PORT}`);
 });
