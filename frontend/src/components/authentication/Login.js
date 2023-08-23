d32b111b176706730dc6c500515409d686c8ed2b
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-23 15:32:19 +0530

    added login-page-ui

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [show, setShow] = useState(false);
+  const [userEmail, setUserEmail] = useState();
+  const [password, setUserPassword] = useState();
 
-export default Login
+  const handleClickBtn = () => {
+    setShow(!show);
+  };
+
+  const submitHandler = () => {};
+  return (
+    <VStack>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter Your Email"
+          onChange={(e) => {
+            setUserEmail(e.target.value);
+          }}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Your Password"
+            onChange={(e) => {
+              setUserPassword(e.target.value);
+            }}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClickBtn}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Sign Up
+      </Button>
+      <Button
+        variant="solid"
+        colorScheme="red"
+        width="100%"
+        onClick={() => {
+          setUserEmail("guest@example.com");
+          setUserPassword("12345");
+        }}
+      >
+        Get Guest User Credentials
+      </Button>
+    </VStack>
+  );
+};
+
+export default Login;
