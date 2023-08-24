74a8c0ec264b0eed66559ab590b8183b36a9b5e2
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-24 13:33:08 +0530

    updated-UserModel

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bycrypt = require("bcryptjs");
 const UserSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
@@ -14,5 +14,17 @@ const UserSchema = mongoose.Schema(
   },
   { timestamp: true }
 );
+
+UserSchema.methods.validatePassword = async function (password) {
+  return await bycrypt.compare(password, this.password);
+};
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+  const salt = await bycrypt.genSalt(10);
+  this.password = await bycrypt.hash(this.password, salt);
+});
 
 module.exports = mongoose.model("UserModel", UserSchema);
